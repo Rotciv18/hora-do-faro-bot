c2071a41dce7c903ef0a8f9a7cf19798fd12f6d4
Victor Batista <v.rotciv@hotmail.com>
2022-06-26 20:45:52 -0300

    disconnecting from channel if bot is alone; removing from queues if removed from guild

--- src/app/jobs/AudioPlayerJob.ts
+++ src/app/jobs/AudioPlayerJob.ts
@@ -3,11 +3,16 @@ import AudioPlayerQueue from '../queues/AudioPlayerQueue';
 import randomIntFromInterval from '../helpers/randomIntFromInterval';
 import { getVoiceConnection } from '@discordjs/voice';
 import PlayAudioService from '../services/connections/PlayAudioService';
+import DiscordClient from '../../DiscordClient';
 
 export default async (job: IConnectToChannelQueue) => {
   const { guildId } = job.data;
   const delay = randomIntFromInterval(300000, 600000);
 
+  const guild = DiscordClient.guilds.cache.get(guildId);
+  if (!guild) {
+    return;
+  }
   const connection = getVoiceConnection(guildId);
 
   if (!connection) {
--- src/app/jobs/ConnectToChannelJob.ts
+++ src/app/jobs/ConnectToChannelJob.ts
@@ -1,14 +1,22 @@
 import IConnectToChannelQueue from '../interfaces/IConnectToChannelQueue';
 import DiscordClient from '../../DiscordClient';
 import GetMostMembersChannel from '../services/channels/GetMostMembersChannel';
-import { joinVoiceChannel, getVoiceConnection } from '@discordjs/voice';
+import {
+  joinVoiceChannel,
+  getVoiceConnection,
+  VoiceConnection,
+} from '@discordjs/voice';
 
 import ConnectToChannelQueue from '../queues/ConnectToChannelQueue';
+import { VoiceChannel } from 'discord.js';
 
 export default async function (job: IConnectToChannelQueue) {
   const { guildId } = job.data;
 
   const guild = DiscordClient.guilds.cache.get(guildId);
+  if (!guild) {
+    return;
+  }
   const channel = GetMostMembersChannel.call(guild);
   const currentConnection = getVoiceConnection(guildId);
 
@@ -17,11 +25,19 @@ export default async function (job: IConnectToChannelQueue) {
     channel &&
     channel.id !== currentConnection?.joinConfig.channelId
   ) {
-    const connection = joinVoiceChannel({
+    joinVoiceChannel({
       guildId: guildId,
       channelId: channel?.id,
       adapterCreator: guild.voiceAdapterCreator,
     });
+  } else if (currentConnection?.joinConfig.channelId) {
+    const myChannel: VoiceChannel | any = guild?.channels.cache.get(
+      currentConnection?.joinConfig.channelId
+    );
+
+    if (myChannel?.members.size <= 1) {
+      currentConnection.disconnect();
+    }
   }
 
   ConnectToChannelQueue.add({ guildId }, { delay: 1000 });
