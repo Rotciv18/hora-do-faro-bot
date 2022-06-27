af73ff1a506e0ffaa04c921d37de75b5284fe5fc
Victor Batista <v.rotciv@hotmail.com>
2022-06-27 11:25:53 -0300

    lmao i'm dumb

--- src/app/queues/RemoveCompletedQueue.ts
+++ src/app/queues/RemoveCompletedQueue.ts
@@ -1,10 +1,9 @@
 import Queue from 'bull';
 
-export default new Queue('AudioPlayerQueue', {
+export default new Queue('RemoveCompletedQueue', {
   redis: {
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT as string),
   },
 });
-
